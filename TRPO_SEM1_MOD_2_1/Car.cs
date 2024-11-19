2fd4c4d0f2b6c4b53d702fafe5b6b6a370b8ab20
Kryshtof Yurii <yura.krishtof@gmail.com>
2024-11-19 13:57:44 +0200

    Add project files.

---
+++ TRPO_SEM1_MOD_2_1/Car.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TRPO_SEM1_MOD_2_1;
+
+public class Car
+{
+    public string Brand { get; set; }
+    public string Number { get; set; }
+    public int Year { get; set; }
+    public decimal Price { get; set; }
+
+    // Конструктор за замовчуванням
+    public Car()
+    {
+    }
+
+    // Конструктор з параметрами
+    public Car(string brand, string number, int year, decimal price)
+    {
+        Brand = brand;
+        Number = number;
+        Year = year;
+        Price = price;
+    }
+}
---
+++ TRPO_SEM1_MOD_2_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace TRPO_SEM1_MOD_2_1;
+
+class Program
+{
+    static void Main()
+    {
+        List<Car> cars = new List<Car>
+        {
+            new Car("Toyota", "AA1111BB", 2020, 25000),
+            new Car("BMW", "BC2222KM", 2021, 35000),
+            new Car("Audi", "AX3333PP", 2019, 30000),
+            new Car("Mercedes", "KA4444XX", 2022, 40000)
+        };
+
+        var numbers = cars.AsParallel()
+                         .Select(car => car.Number);
+
+        // Виводимо номери
+        Console.WriteLine("Номери автомобiлiв:");
+        foreach (var number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
